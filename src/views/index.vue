861e2c6396d909ad37f7d766f33f730ddb9e1f9f
qqkillqq <qqkillqq@163.com>
2020-08-03 19:58:43 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,13 +4,31 @@
       src="https://zhang-yue.oss-cn-beijing.aliyuncs.com/bingshan/1212.png"
       alt=""
     />
-
-    <div class="bg-white radius4 padding20 margin20">
-      <router-link to="/about"> header设置案例 </router-link>
-    </div>
-    <div class="bg-white radius4 padding20 margin20">
-      <router-link to="/scrollLoad"> 滚动加载案例 </router-link>
-    </div>
+    <router-link to="/about">
+      <gc-cell title="header" class="radius3 margin20"
+        >一个简单的页面head设置</gc-cell
+      >
+    </router-link>
+    <router-link to="/scrollLoad">
+      <gc-cell title="滚动加载" class="radius3 margin20"
+        >展示如何在页面中使用滚动加载</gc-cell
+      >
+    </router-link>
+    <router-link to="/cell">
+      <gc-cell title="cell" class="radius3 margin20"
+        >一个经常在列表页面用到的通用样式</gc-cell
+      >
+    </router-link>
+    <router-link to="/gradient">
+      <gc-cell title="渐变" class="radius3 margin20"
+        >这里展示了CMUI中自带的334种渐变</gc-cell
+      >
+    </router-link>
+    <router-link to="/login">
+      <gc-cell title="系统登录" class="radius3 margin20"
+        >通用系统登录DEMO</gc-cell
+      >
+    </router-link>
   </div>
 </template>
 <script>
---
+++ src/views/login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class=" clearfix login "
+    :class="`gradient_${gradientNumber}`"
+    style="height:100vh"
+  >
+    <div class="topIcon" @click="gradientNumber = (Math.random() * 334) | 0">
+      K
+    </div>
+    <cmui-form>
+      <cmui-form-item label="账号">
+        <cmui-input target-class="reverse"></cmui-input>
+      </cmui-form-item>
+      <cmui-form-item label="密码">
+        <cmui-input target-class="reverse" type="password"></cmui-input>
+      </cmui-form-item>
+      <span
+        class="btn block  margint50 "
+        style="background:transparent"
+        @click="login"
+        >登录</span
+      >
+    </cmui-form>
+  </div>
+</template>
+<script>
+let maple = window.maple;
+export default {
+  mate: {
+    headerConfig: {
+      title: "系统登录",
+      show: false
+    }
+  },
+  data() {
+    return {
+      gradientNumber: (Math.random() * 334) | 0
+    };
+  },
+  methods: {
+    login() {
+      maple.alert("登录成功");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.login {
+  /deep/ {
+    .cmui-form-item__label {
+      color: rgb(222, 222, 222);
+    }
+    input {
+      color: white;
+    }
+    .cmui-form {
+      padding: 30px;
+      max-width: 500px;
+      margin: 0 auto;
+    }
+  }
+}
+.topIcon {
+  width: 150px;
+  height: 150px;
+  border: 1px solid white;
+  border-radius: 150px;
+  line-height: 150px;
+  color: white;
+  text-align: center;
+  font-size: 80px;
+  margin: 40px auto;
+}
+</style>
